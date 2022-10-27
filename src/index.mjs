d120daca0c60089dfc55dac4569b835703ecdd3c
ced <cedrickcampoto@yahoo.com>
2022-10-27 09:44:03 +0800

    update

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,7 @@
 
 
-window.API_URL = 'http://localhost:9324';
+// window.API_URL = 'http://localhost:9324';
+window.API_URL = 'http://18.143.188.232:49153';
 
 import './router'
 import './component_build/index.js'
